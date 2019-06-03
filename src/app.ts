319f9794e6b1c66cd7e188abe41e8e1abf2355bd
mnebuerquo <mnebuerquo@users.noreply.github.com>
2019-06-03 19:53:15 -0400

    upgraded some packages, refactored app init

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,29 @@
 // Require the framework and instantiate it
 import * as fastify from 'fastify'
+import * as favicon from 'fastify-favicon'
 import * as helmet from 'fastify-helmet'
 import routes from './routes'
+import addSwagger from './swagger'
 
 const server = fastify({
   logger: true,
 })
 
-import addSwagger from './swagger'
 addSwagger(server)
 
 server.register(helmet, { hidePoweredBy: { setTo: 'your mom' } })
-
+server.register(favicon)
 server.register(routes, {})
 
+const fatal = err => {
+  server.log.error(err)
+  process.exit(1)
+}
+
 // Run the server!
-server.listen(8080, '0.0.0.0', (err, address) => {
-  if (err) {
-    server.log.error(err)
-    process.exit(1)
-  }
-  server.log.info(`server listening on ${address}`)
-})
+server
+  .listen(8080, '0.0.0.0')
+  .then(address => {
+    server.log.info(`server listening on ${address}`)
+  })
+  .catch(fatal)
